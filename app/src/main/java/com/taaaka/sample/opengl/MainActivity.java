b127f6925e9f428b4fd95d87e76301dc9d9d548c
taaaka <Rw.taaaka@gmail.com>
2017-10-17 21:22:42 +0900

    [modify] add GLView to Activity

--- app/src/main/java/com/taaaka/sample/opengl/MainActivity.java
+++ app/src/main/java/com/taaaka/sample/opengl/MainActivity.java
@@ -3,14 +3,20 @@ package com.taaaka.sample.opengl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.taaaka.test.mygl.MyGLSurfaceView;
+
 /**
  * Created by taaaka on 2017/10/07.
  */
 public class MainActivity extends AppCompatActivity {
 
+    private MyGLSurfaceView mGLView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
     }
 }
--- mygl-module/src/main/java/com/taaaka/test/mygl/MyGLSurfaceView.java
+++ mygl-module/src/main/java/com/taaaka/test/mygl/MyGLSurfaceView.java
@@ -3,15 +3,21 @@ package com.taaaka.test.mygl;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 
+import com.taaaka.test.mygl.renderer.MyGLRenderer;
+
 /**
  * Created by taaaka on 2017/10/07.
  */
 
 public class MyGLSurfaceView extends GLSurfaceView {
 
+    private MyGLRenderer mRenderer;
+
     public MyGLSurfaceView(Context context) {
         super(context);
 
+        mRenderer = new MyGLRenderer();
+        setRenderer(mRenderer);
     }
 
     /**
